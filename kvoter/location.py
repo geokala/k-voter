08c3d82a0fd4214457575cc8ce5c01d97ebbd8bd
Madda <nobody.youknow@local>
2015-01-18 11:29:55 +0000

    Move location listing logic to utils

--- kvoter/location.py
+++ kvoter/location.py
@@ -2,15 +2,9 @@ from flask.ext.login import login_required, current_user
 from kvoter.db import Location, User
 from flask import request, render_template, redirect, url_for, flash
 from wtforms import Form, TextField, validators, SelectField
-from kvoter.utils import user_not_authorised_to
-
-
-def int_or_null(data):
-    if data == 'None':
-        return None
-    else:
-        print('not none')
-        return int(data)
+from kvoter.utils import (user_not_authorised_to,
+                          int_or_null,
+                          get_authorised_locations)
 
 
 class LocationForm(Form):
@@ -29,67 +23,15 @@ class LocationForm(Form):
 
 @login_required
 def create_location_view():
-    locations_admin = User.query.filter(
-        User.id == current_user.id,
-    ).one().locations_admin
-    if len(locations_admin) == 0 and not current_user.is_admin:
-        # A standard user with no roles cannot be authorised to create any
-        # locations
-        return user_not_authorised_to('create locations')
-
-    allowed_locations = [location.id for location in locations_admin]
-
-    # Getting all locations means more memory usage but only one trip to the
-    # DB. The alternative is multiple trips later, which is likely to cause
-    # 'interesting' performance penalties with more than a few locations.
-    # See location parent mapping for display_locations
-    all_locations = {
-        location.id: {
-            'name': location.name,
-            'parent': location.parent_location_id,
-        }
-        for location in Location.query.all()
-    }
-
-    can_create_top_level = False
-    if current_user.is_admin:
-        allowed_locations = all_locations
-        can_create_top_level = True
-    elif len(allowed_locations) > 0:
-        allowed_locations = {location: details
-                             for location, details in all_locations.items()
-                             if location in allowed_locations}
-    else:
-        # With no location_admin or admin privs, the user cannot create
-        # locations
-        return user_not_authorised_to('create locations')
-
-    # Format the allowed locations in a useful manner
-    allowed_locations = [(location, details['name'])
-                         for location, details in allowed_locations.items()]
-
-    if can_create_top_level:
-        allowed_locations.append((None, '-'))
+    allowed_locations = get_authorised_locations()
+    if 'error' in allowed_locations.keys():
+        return allowed_locations['error']
 
     form = LocationForm(request.form)
-    # Make the locations easier to read
-    display_locations = []
-    for location in allowed_locations:
-        location_id = location[0]
-        location_name = []
-        next_id = location_id
-        while next_id is not None:
-            location_name.append(all_locations[next_id]['name'])
-            next_id = all_locations[next_id]['parent']
-        location_name = '/'.join(location_name)
-        if location_name == '':
-            location_name = '-'
-        display_locations.append((location_id, location_name))
-
-    form.parent_location.choices = display_locations
+    form.parent_location.choices = allowed_locations['display']
     if request.method == 'POST' and form.validate():
         parent_location_name = None
-        for location in allowed_locations:
+        for location in allowed_locations['basic']:
             if form.parent_location.data == location[0]:
                 parent_location_name = location[1]
                 break
